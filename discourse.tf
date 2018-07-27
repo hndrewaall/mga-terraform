c071f4248a7f562c101de09d8040f7e8574bee27
Andrew Hall <hndrewaall@gmail.com>
2018-07-27 13:58:36 -0700

    Drop discourse instance and address

--- discourse.tf
+++ discourse.tf
@@ -1,26 +1,3 @@
-resource "aws_instance" "discourse" {
-    ami = "ami-60b6c60a"
-    instance_type = "t2.small"
-
-    tags = {
-        Name = "Discourse"
-    }
-
-    monitoring = true
-    vpc_security_group_ids = [
-      "${aws_security_group.web-prod.id}",
-      "${aws_security_group.ssh-gbre.id}",
-      "${aws_security_group.outbound-all.id}"
-    ]
-}
-
-module "discourse_address" {
-  source = "modules/addr"
-  name = "discourse"
-  instance_id = "${aws_instance.discourse.id}"
-  zone_id = "${aws_route53_zone.root.id}"
-}
-
 resource "aws_s3_bucket" "discourse_uploads" {
     bucket = "massgo-discourse-uploads"
     versioning { enabled = true }
